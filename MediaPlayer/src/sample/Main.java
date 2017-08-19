e42fcd8501ecb6b6b30e500b7f9509dbdecabd25
Bhavesh-Joshi <bhavesh.it21@gmail.com>
2017-08-19 08:18:30 +0530

    Commit

---
+++ MediaPlayer/src/sample/Main.java
@@ -0,0 +1,10 @@
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class Main{
+    public static void main(String... args){
+        launch(args);
+    }
+    public void start(Stage primary)
+}
